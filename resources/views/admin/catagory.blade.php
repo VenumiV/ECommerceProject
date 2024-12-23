f699d1463999fe0d3447de0889c2634d37321c91
Kirula Upadya <kirulapadukka@gmail.com>
2024-12-24 00:19:46 +0530

    Created adding catagory page with AdminController

---
+++ resources/views/admin/catagory.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  @include('admin.css')
+  <style type="text/css">
+    .div_center {
+      text-align: center;
+      padding-top: 40px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_sidebar.html -->
+    @include('admin.sidebar')
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_navbar.html -->
+      @include('admin.header')
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="div_center">
+            <h2>Add Catagory</h2>
+          </div>
+        </div>
+      </div>
+      <!-- container-scroller -->
+      <!-- plugins:js -->
+      <script src="admin/assets/vendors/js/vendor.bundle.base.js"></script>
+      <!-- endinject -->
+      <!-- Plugin js for this page -->
+      @include('admin.script')
+      <!-- End custom js for this page -->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 
 /*Route::get('/', function () {
@@ -24,16 +25,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
 });
 
 
-Route::middleware(['auth:sanctum' , 'verified'])->get('/dashboard' , function () {
-    return view ('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
 })->name('dashboard');
- 
+
 route::get('/redirect', [HomeController::class, 'redirect']);
+
+route::get('/view_catagory', [AdminController::class, 'view_catagory']);
 
 
 //require __DIR__.'/auth.php';
-

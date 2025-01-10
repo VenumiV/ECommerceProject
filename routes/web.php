4fb1c4960b9ab6f9b2e0d41eb4475b2e7cea8c7a
ashandissa <155371268+ashandissa@users.noreply.github.com>
2025-01-10 12:04:45 +0530

    Show product in cart and cart data to order table

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,8 @@ route::get('/product_details/{id}', [HomeController::class, 'product_details']);
 
 route::post('/add_cart/{id}', [HomeController::class, 'add_cart']);
 
-route::get('/sjow_cart/{id}', [HomeController::class, 'show_cart']);
+route::get('/show_cart/{id}', [HomeController::class, 'show_cart']);
 
+route::get('/remove_cart/{id}', [HomeController::class, 'remove_cart']);
 
 //require __DIR__.'/auth.php';

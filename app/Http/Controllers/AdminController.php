ac347737dff16f431f1aeb7e0c132f64fff82a1f
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-03-20 13:29:46 +0530

    Admin can Send a Email notification to the customer

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,6 +12,10 @@ use App\Models\order;
 
 use PDF;
 
+use Notification;
+
+use App\Notifications\SendEmailNotification;
+
 class AdminController extends Controller
 {
     public function view_catagory()
@@ -134,4 +138,27 @@ class AdminController extends Controller
         $pdf = PDF::loadView('admin.pdf', compact('order'));
         return $pdf->download('order_details.pdf');
     } 
+
+    public function send_email($id){
+        $order = order::find($id);
+        return view('admin.email_info',compact('order'));
+    }
+
+    public function send_user_email(Request $request, $id){
+        $order=order::find($id);
+
+        $details= [
+            'greeting'=> $request->greeting,
+            'firstline'=> $request->firstline,
+            'body'=> $request->body,
+            'button'=> $request->button,
+            'url'=> $request->url,
+            'lastline'=> $request->lastline,
+
+        ];
+
+        Notification::send($order,new SendEmailNotification($details));
+
+        return redirect()->back();
+    }
 }

9d7fb10dce3b2782aac9d4d5d8629917d552ec47
ashandissa <155371268+ashandissa@users.noreply.github.com>
2025-03-28 13:41:10 +0530

    Display Admin Dashboard Data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,25 @@ class HomeController extends Controller
             $usertype = Auth::user()->usertype;
         
             if ($usertype === '1') {
-                return view('admin.home');
+                $total_product=product::all()->count();
+                $total_order=order::all()->count();
+                $total_user=user::all()->count();
+                $order=order::all();
+
+                $total_revenue=0;
+
+        
+                foreach($order as $order)
+                {
+                    $total_revenue=$total_revenue + $order->price;  
+
+
+                }
+
+                $total_delivered=order::where('delivery_status','=','delivered')->get->count();
+
+
+                return view('admin.home',compact('total_product','total_order','total_user','total_revenue','total_delivered'));
             } else {
                 
                 $product=Product::paginate(9);
